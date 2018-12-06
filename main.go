d0a9b27dcd97e3b8a616854f00946e53f8f81211
srttk <saroteki26@gmail.com>
2018-12-06 22:26:58 +0900

    WithDeadline の実装

--- main.go
+++ main.go
@@ -10,7 +10,8 @@ import (
 
 // 終わらない処理
 func leak(ctx context.Context) {
-	child, _ := context.WithCancel(ctx)
+	child, cancel := context.WithCancel(ctx)
+	defer cancel()
 	for {
 		time.Sleep(1 * time.Second)
 		fmt.Println("looping...")
@@ -19,21 +20,25 @@ func leak(ctx context.Context) {
 		case <-child.Done():
 			fmt.Println("break loop.")
 			return
+		default:
+			continue
 		}
 	}
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
+	// WithDeadline 用の time.Timeを作成
+	// 現在時刻の3秒後を deadline に設定
+	deadline := time.Now().Add(3 * time.Second)
 	// 親 context 作成
-	parent, cancel := context.WithCancel(context.Background())
+	parent, cancel := context.WithDeadline(context.Background(), deadline)
+	// 上記設定した時間が来たら cancel() を呼ぶ
+	defer cancel()
 
 	// ゾンビ goroutine に ctx を渡す
 	go leak(parent)
 
-	fmt.Fprintf(w, "allow request.")
-
-	// リクエスト返したらゾンビを殺す
-	cancel()
+	fmt.Fprint(w, "allow request.")
 
 	select {
 	case <-parent.Done():
