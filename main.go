eb848e7950d3152e98bbcf28ce0c2465ff2e4326
srttk <saroteki26@gmail.com>
2018-12-06 19:32:13 +0900

    with time out cancel

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,18 +9,37 @@ import (
 )
 
 // 終わらない処理
-func leak() {
+func leak(ctx context.Context) {
+	child, cancel := context.WithCancel(ctx)
+	defer cancel()
 	for {
 		time.Sleep(1 * time.Second)
 		fmt.Println("looping...")
+
+		select {
+		case <-child.Done():
+			fmt.Println("break loop.")
+			return
+		}
 	}
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	// ゾンビ goroutine
-	go leak()
+	// 親 context 作成
+	parent, cancel := context.WithCancel(context.Background())
+
+	// ゾンビ goroutine に ctx を渡す
+	go leak(parent)
 
 	fmt.Fprintf(w, "allow request.")
+
+	// リクエスト返したらゾンビを殺す
+	cancel()
+
+	select {
+	case <-parent.Done():
+		fmt.Println(parent.Err())
+	}
 }
 
 func main() {
